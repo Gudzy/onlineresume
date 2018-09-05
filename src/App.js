b4e012d5cf718d0841bad9cd3ec37cdf151e70f6
Gustav Dyngeseth <gustdyn@gmail.com>
2018-09-05 23:01:17 +0200

    Adding footer image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import pb from './pb.jpg';
+import pb2 from './pb2.png';
+import pb3 from './GD_PB.jpg';
+import pb4 from './pb4.png';
+import cv from './GD_CV.pdf';
+import spire from './spirelogo.png';
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +15,8 @@ class App extends Component {
       home: true,
       about: false,
       contact: false,
-
+      cvnor: false,
+      cveng: false
     };
   }
 
@@ -45,7 +51,6 @@ class App extends Component {
             }}><i class="fa fa-home"></i> Home</a>
           <a className={this.state.contact? "active" : ""} onClick={e => {
               e.preventDefault();
-
               this.scrollDown();
             }}>Contact</a>
           <a className={this.state.about? "active" : ""} onClick={e => {
@@ -79,44 +84,72 @@ class App extends Component {
             }}>About</button>
         </div>
       </div>
+
+      <div className="whitespace">
+      </div>
+
       </div> : ""
     );
   }
 
-  renderContact() {
-    return(this.state.contact?
-      <div>
-        <p>Lets talk!</p>
-        <p> e-mail: gustdyn@gmail.com</p>
-        <p>tlf: +47 40 01 92 30</p>
-      </div>
-    : "");
-  }
+ 
 
 
   renderAbout() {
     return(this.state.about?
-      <div className="card">
-        <img src={pb} alt="Gustav"></img>
-        <h4>Gustav Dyngeseth</h4>
-        <p className="title">IT Consultant</p><p className="title">Computer Science student - Norwegian University of Science and Technology</p>
-        <div id="myProgress">
-  <div id="myBar">40%</div>
-</div>
-        <p className="title"></p>
-        <a href="https://drive.google.com/file/d/1tDqfFWaM9YCJAaLQHSNDT-BtDa-5_3Ai/view"><button class="btn"><i class="fa fa-download"></i>Resume</button></a>
-        <a href="https://drive.google.com/file/d/1gfqw0caDmyvBKGifDveqkaTn-eeU-eXQ/view"><button class="btn"><i class="fa fa-download"></i>Transcript</button></a>
+      <div>
+        <div className="card">
+          <img src={pb} alt="Gustav"></img>
+          <h4>Gustav Dyngeseth</h4>
+          <p className="title">IT Consultant</p><p className="title">Computer Science student - Norwegian University of Science and Technology</p>
+          <div id="myProgress">
+            <div id="myBar">40%</div>
+          </div>
+          <p className="title"></p>
+          <a onClick={e => {
+                e.preventDefault();
+                this.setState({ cvnor: !this.state.cvnor
+                });
+              }}><button class="btn"><i class="fa fa-show"></i>Curriculum Vitae</button></a>
+        </div>
+        {this.renderNorCV()}
       </div>
     : "");
   }
+
+  renderNorCV() { 
+    return (
+    this.state.cvnor ?
+      <object className="pdf" data={cv}>
+      </object>
+      : ""
+    );
+  }
+
+  renderEngCV() {
+    return (
+      this.state.cveng ?           
+      <object class="pdf" data={cv} width="100%" >
+      </object> : ""
+
+    );
+  }
+
+
 
   renderFooter() {
     return(
       <div id="footer">
         <div class="footer-social-icons">
-            <h4 class="_14">Find me on</h4>
-            <ul class="social-icons">
+            <h4 class="_14">Contact me</h4>
+            <picture >
+              <img  src={pb4} alt="Flowers" className="footerimg"/>
+            </picture>
+            <p>gustav.dyngeseth@spireconsulting.no</p>
+            <p>+ 47 400 19 230</p>
+            <ul class="social-icons">   
                 <li><a href="https://www.linkedin.com/in/gustavdyngeseth/" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
+                <li><a href="https://github.com/Gudzy" class="social-icon"> <i class="fa fa-github"></i></a></li>
                 <li><a href="https://www.facebook.com/GDyngeseth" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
                 <li><a href="https://www.instagram.com/gustavd/" class="social-icon"> <i class="fa fa-instagram"></i></a></li>
             </ul>
